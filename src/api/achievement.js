3195479ca01b5dd40d4ef15e8d1a2be72575e700
mohan <mohanli2001@outlook.com>
2023-04-25 09:39:01 +0800

    feat: 科研成果显示全部连接后端成功

--- src/api/achievement.js
+++ src/api/achievement.js
@@ -1,7 +1,7 @@
 import request from "../util/request.js";
 export function getModule() {
   return request({
-    url: "/achievementsNode.html",
+    url: "/achievementsNode",
     method: "get",
   });
 }
